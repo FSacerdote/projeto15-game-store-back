6910f19081f430a9a2f2efa901489b121e8f61cd
anajbsouza <anaju.batsouza@gmail.com>
2023-07-12 20:52:28 -0300

    feat: informações de usuário

---
+++ src/controllers/users.controllers.js
@@ -0,0 +1,47 @@
+import bcrypt from "bcrypt";
+import { v4 as uuid } from "uuid";
+import { db } from "../database/database.connection.js";
+
+export async function signup(req, res) {
+    const { name, email, password } = req.body;
+    
+    try {
+        const usuario = await db.collection("users").findOne({ email })
+        if (usuario) return res.status(409).send("E-mail já cadastrado")
+
+        const hash = bcrypt.hashSync(password, 10);
+
+        await db.collection("users").insertOne({ name, email, password: hash });
+        res.sendStatus(201);
+    } catch(err) {
+        res.status(500).send(err.message);
+    }
+}
+
+export async function login(req, res) {
+    const { email, password } = req.body;
+
+    try {
+        const usuario = await db.collection("users").findOne({ email });
+        if (!usuario) return res.status(401).send("E-mail não cadastrado");
+
+        const correctPassword = bcrypt.compareSync(password, usuario.password);
+        if (!correctPassword) return res.status(401).send("Senha incorreta");
+
+        const token = uuid();
+        await db.collection("sessions").insertOne({token, userId: usuario._id});
+        res.send({token, userName: usuario.name});
+    } catch(err) {
+        res.status(500).send(err.message);
+    }
+}
+
+export async function logout(req, res) {
+    const { token } = res.locals.sessions;
+    try {
+        await db.collection("sessions").deleteOne({ token })
+        res.sendStatus(200);
+    } catch(err) {
+        res.status(500).send(err.message);
+    }
+}
