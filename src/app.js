825f1069b2041b17c1c342ca7258df3f855d3f0c
Fernando_Sacerdote <fernando.saerdote@gmail.com>
2023-07-11 21:48:04 -0300

    build: estrutura do backend

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import dotenv from "dotenv"
+import express from "express"
+import cors from "cors"
+import router from "./routes/index.routes.js"
+
+dotenv.config()
+const port = process.env.PORT || 5000
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+app.use(router)
+
+app.listen(port, ()=> console.log(`Servidor rodando na porta ${port}`))
