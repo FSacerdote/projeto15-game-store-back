0d7732436bdb0afafd8cb966ba84343ff06c784d
alyhenr <alyssonhra99@gmail.com>
2023-07-17 16:43:24 -0300

    feat: checkout

--- src/middlewares/cartValidation.js
+++ src/middlewares/cartValidation.js
@@ -1,12 +1,16 @@
 export default (schema) => {
     return (req, res, next) => {
-        const { token } = req.headers;
-        const data = req.body;
+        const token = req.headers.authorization.replace("Bearer ", "");
+        const data = req.body.userInfo;
+        const items = req.body.selectedItems;
 
         const validation = schema(data);
         if (validation.error) {
             return res.status(422).send(validation.error.details.map(detail => detail.message));
         }
+
+        res.locals.userInfo = data;
+        res.locals.items = items;
 
         if (token) {
             res.locals.isLoggedIn = true;
